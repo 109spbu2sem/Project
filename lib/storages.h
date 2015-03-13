ff2d983fa7a076435681c18681a847d94a31b0e9
Vetcher <vetcher@ngs.ru>
2015-03-13 21:04:06 +0300

    add Arc, Angle classes, some fixes

--- lib/storages.h
+++ lib/storages.h
@@ -1,44 +1,6 @@
 #ifndef STORAGES_H
 #define STORAGES_H
 
-class Vector
-{
-private:
-public:
-	double x;
-	double y;
-	Vector(double _x, double _y)
-	{
-		x = _x;
-		y = _y;
-	};
-	Vector()
-	{
-		x = 0;
-		y = 0;
-	};
-	Vector operator+ (const Vector &v);
-	void operator+= (const Vector &v);
-	Vector operator- (const Vector &v);
-	void operator-= (const Vector &v);
-	Vector operator* (const Vector &v);//scalar
-	Vector operator* (const double &a);
-	Vector operator*= (const double &a);
-	double length();//returns length
-	double module();//returns length
-	Vector unit_v();//returns unit vector
-};
-
-struct Point
-{
-	double x;
-	double y;
-};
-struct Segment
-{
-	Point o;//begin
-	Vector d;//direction
-};
 //------------------------------------------------------------------------
 //----------------------------------LIST---------------------------------
 //------------------------------------------------------------------------
---
+++ lib/structures.h
@@ -0,0 +1,76 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+class Vector
+{
+private:
+public:
+	double x;
+	double y;
+	Vector(double _x, double _y)
+	{
+		x = _x;
+		y = _y;
+	};
+	Vector()
+	{
+		x = 0;
+		y = 0;
+	};
+	Vector operator+ (const Vector &v);
+	void operator+= (const Vector &v);
+	Vector operator- (const Vector &v);
+	void operator-= (const Vector &v);
+	Vector operator* (const Vector &v);//scalar
+	Vector operator* (const double &a);
+	Vector operator*= (const double &a);
+	double length();//returns length
+	double module();//returns length
+	Vector unit_vector();//returns unit vector
+};
+
+class Angle
+{
+public:
+	double angle;
+
+	Angle(double a = 0) { angle = a; }
+	double grads();
+};
+
+class Arc
+{
+public:
+	Point o;
+	Vector d;
+	Angle angle;
+
+	Arc()
+	{
+		o = { 0, 0 };
+		d = { 0, 0 };
+		angle = 0;
+	}
+	Arc(Point &p, Vector &z, Angle &a)
+	{
+		o = p;
+		d = z;
+		angle = a;
+	}
+	double length();
+	double area();
+	Segment chord();
+};
+
+struct Point
+{
+	double x;
+	double y;
+};
+struct Segment
+{
+	Point o;//begin
+	Vector d;//direction
+};
+
+#endif
