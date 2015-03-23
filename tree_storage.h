7a89ebd8614f474181d76b1074efb3750f7fe46a
Alexey Yatsyshin <lexayat@mail.ru>
2015-03-23 16:53:05 +0300

    [2]

---
+++ tree_storage.h
@@ -0,0 +1,75 @@
+#ifndef _TREESTOR_H
+#define _TREESTOR_H
+template<typename A, typename B> class tree_storage
+{
+	struct cell{
+		cell *left;
+		cell *right;
+		A a;
+		B b;
+	};
+	cell *_root;
+public:
+	tree_storage(){ _root = 0; };
+
+	void DelForDest(cell *r)
+	{
+		if (r->left != 0)DelForDest(r->left);
+		else delete r->left;
+		if (r->right != 0)DelForDest(r->right);
+		else delete r->right;
+		delete r;
+	}
+	~tree_storage()
+	{
+		DelForDest(_root);
+	};
+
+	void add(const A& a, const B& b){
+		if (_root == 0){
+			_root = new cell;
+			_root->a = a;
+			_root->b = b;
+			_root->left = _root->right = 0;
+		}
+		else{
+			cell *newcell = new cell;
+			newcell->a = a;
+			newcell->b = b;
+			newcell->left = 0;
+			newcell->right = 0;
+
+			cell *cur = _root;
+			cell *parent = 0;
+			while (cur){
+				if (cur->a == a) cur->b = b;
+				parent = cur;
+				if (cur->a < a)
+					cur = cur->right;
+				else cur = cur->left;
+			}
+			if (parent->a < a) parent->right = newcell;
+			else parent->left = newcell;
+		}
+	}
+	bool hasA(const A& a){
+		cell *cur = _root;
+		while (cur){
+			if (cur->a == a) return true;
+			if (cur->a < a) cur = cur->right;
+			else cur = cur->left;
+		}
+		return false;
+	};
+	B& getBbyA(const A& a){
+		cell *cur = _root;
+		while (cur){
+			if (cur->a == a) return cur->b;
+			if (cur->a < a) cur = cur->right;
+			else cur = cur->left;
+		}
+		throw 1;
+	};
+};
+
+#endif
