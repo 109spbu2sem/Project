dc0dc6c046f2dc336d9251b4c38fee1cf6118390
PrideBradley <plat96@yandex.ru>
2015-03-31 00:38:38 +0400

    Fyxaga

--- BinaryTree.h
+++ BinaryTree.h
@@ -8,30 +8,49 @@ template<typename A, typename B> class BinaryTree
 		B b;
 		cell*right;
 		cell*left;
+		cell*parent;
 	};
 cell*_root;
 cell*_cur;
+int height(cell*);
+int heightdiff(cell*);
+void rotCCW(cell*);
+void rotCCW2(cell*);
 public:
 	BinaryTree() {
 		_root = 0;
 		_cur = 0;
 	}
 	~BinaryTree() {
+		_cur = _root;
 		cell*parent = _cur;
-		while (_cur) {
-			while (_cur->left)
+		while (parent) {
+			while (_cur->left) {
+				parent = _cur;
 				_cur = _cur->left;
-			while (_cur->right)
-				_cur = _cur ->right;
+			}
+			while (_cur->right) {
+				parent = _cur;
+				_cur = _cur->right;
+			}
 			if (!_cur->left && !_cur->right) {
-				delete[] _cur;
+				if (parent->left == _cur) {
+					delete[] _cur;
+					parent->left = 0;
+				}
+				else {
+					delete[] _cur;
+					parent->right = 0;
+				}
+				if (_cur == _root)
+					break;
 				_cur = _root;
 			}
 		}
 	}
 	void add(A&, B&);
 	bool hasA(A&);
-	B & getBbyA(A);
+	B&  getBbyA(A&);
 };
 template<typename A, typename B> void BinaryTree<A,B>::add(A&a, B&b){
 	if (_root == 0) {
@@ -51,12 +70,12 @@ template<typename A, typename B> void BinaryTree<A,B>::add(A&a, B&b){
 		_cur = _root;
 		while (_cur) {
 			if (_cur->a == a)
-				_cur->b == b;
+				_cur->b = b;
 			parent = _cur;
 			if (_cur->a < a)
 				_cur = _cur->right;
-			if (_cur->a > a)
-				_cur = _cur->left
+			else
+				_cur = _cur->left;
 		}
 		if (parent->a < a)
 			parent->right = newcell;
@@ -71,22 +90,69 @@ template<typename A, typename B> bool BinaryTree<A, B>::hasA(A&a) {
 			true;
 		if (_cur->a < a)
 			_cur = _cur->right;
-		if (_cur->a > a)
+		else
 			_cur = _cur->left;
 	}
 	return false;
 }
-template<typename A, typename B> B&getBbyA(A a) {
+template<typename A, typename B>B& BinaryTree<A, B>::getBbyA(A& a) {
 	_cur = _root;
 	while (_cur) {
 		if (_cur->a == a)
 			return _cur->b;
 		if (_cur->a < a)
 			_cur = _cur->right;
-		if (_cur->a > a)
+		else
 			_cur = _cur->left;
 	}
-	throw std::invalid_argument("Nooooope");
+}
+template<typename A, typename B> int BinaryTree<A, B>::height(cell*c) {
+	if (c == 0)
+		return 0;
+	return 1 + max(height(c->left), height(c->right))
+}
+template<typename A, typename B> int BinaryTree<A, B>::heightdiff(cell*c) {
+	if (c == 0)
+		return 0;
+	return height(c->left) - height(c->right)
+}
+template<typename A, typename B> void BinaryTree<A, B>::rotCCW(cell*c1) {
+	if (c1 == 0) return;
+	cell *parent = c1->parent;
+	cell*c2 = c1->right;
+	if (c2 == 0) return;
+	cell *T2 = c2->left;
+	c1->right = T2;
+	T2->parent = c1;
+	c2->left = c1;
+	c1->parent = c2;
+	c2->parent = parent;
+	if (parent) {
+		if (parent->right == c1)
+			parent->right = c2;
+		else
+			parent->left = c2;
+	}
+}
+template<typename A, typename B> void BinaryTree<A, B>::rotCCW2(cell*c1){
+	if (c1 == 0)
+		return;
+	cell*parent = c1->parent;
+	cell*c2 = c1->left;
+	if (c2 == 0)
+		return;
+	cell*T2 = c2->right;
+	c1->left = T2;
+	T2->parent = c1;
+	c2->right = c1;
+	c1->parent = c2;
+	c2->parent = parent;
+	if (parent) {
+		if (parent->right == c1)
+			parent->right = c2;
+		else
+			parent->left = c2;
+	}
 }
 #endif 
 
