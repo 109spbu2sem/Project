19d1e35c0b2f7288f3090c7edf7c1a51dc7e2c06
TonyKat <anton.kalaturskii@yandex.ru>
2015-04-07 01:55:47 +0300

    Distance from Point to Section

---
+++ Primitive/DistanceNEW2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "DistanceFromPointToSection.h"
+
+int main()
+{
+	return 0;
+}
---
+++ Primitive/global.h
@@ -0,0 +1,15 @@
+#ifndef PI
+#define PI 3.1415926535897932384
+
+double length(double, double, double, double);
+double DistancePointSection(double,double,double,double,double,double,double);
+
+class IConstraint
+{
+public:
+	virtual double error() = 0;
+	virtual double diff(double *par) = 0;
+};
+
+
+#endif
