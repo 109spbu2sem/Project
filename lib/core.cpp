80a0376d9d67b9237362645c94e757461f3900c5
vetcher <vetcher@ngs.ru>
2015-04-14 02:38:56 +0300

    add calculating functions, add constraint collector, ideological changes

--- lib/core.cpp
+++ lib/core.cpp
@@ -1,5 +1,10 @@
 #include "core.h"
 #include "gui.h"
+#include "constraints\Collector.h"
+#include "constraints\DistancePoint-Point.h"
+#include "constraints\DistancePoint-Section.h"
+#include "constraints\DistanceToTheLine.h"
+#include "constraints\AngleSegment-Segment.h"
 
 CORE::CORE()
 {
@@ -9,6 +14,61 @@ CORE::CORE()
 CORE::~CORE()
 {
 	
+}
+
+void CORE::Redraw()
+{
+	for (ArrayViewer<Point> i(_storage_of_points); i.canMoveNext(); i.moveNext())
+	{
+		mygui->Draw(*i.getValue()._x, *i.getValue()._y);
+	}
+	for (ArrayViewer<Segment> i(_storage_of_segments); i.canMoveNext(); i.moveNext())
+	{
+		mygui->Draw(*i.getValue()._p1->_x, *i.getValue()._p1->_y, *i.getValue()._p2->_x, *i.getValue()._p2->_y);
+	}
+	for (ArrayViewer<Circle> i(_storage_of_circles); i.canMoveNext(); i.moveNext())
+	{
+		mygui->Draw(*i.getValue()._o->_x, *i.getValue()._o->_y, *i.getValue()._r);
+	}
+	return;
+}
+
+void CORE::MainCalculate()
+{
+	ConstraintCollector collector;
+	Storage_Array<double*> parameters;
+	for (ListViewer<IConstraint> i; i.canMoveNext(); i.moveNext())
+	{
+		collector.addConstraint(&i.getValue());
+	}
+	BuildFigure(&collector, &parameters);
+}
+
+void CORE::BuildFigure(IConstraint* constraint, Storage_Array<double*>* parameters)
+{
+	if (!constraint || !parameters) return; // check for true work
+
+	const double f_epsi = 1e-12;
+	double current = constraint->error();
+	double previous = 0;
+	double *dx = new double[parameters->size()];
+
+	do
+	{
+		previous = current;
+
+		double step_min = 0.000001;
+		double step_max = 1000;
+
+		for (unsigned i = 0; i < parameters->size(); i++)
+			dx[i] = -step_min * constraint->diff((*parameters)[i]); // calculate gradient
+		for (unsigned i = 0; i < parameters->size(); i++)
+			*(*parameters)[i] += dx[i];										 // change parametrs
+
+		current = constraint->error();
+	} while (abs(previous - current) > f_epsi);
+	delete[] dx;
+	return;
 }
 
 void CORE::AddObject(double point_x, double point_y)
@@ -37,30 +97,76 @@ void CORE::AddObject(double point_x, double point_y, double radius)
 	_storage_of_circles.add(c);
 }
 
+void CORE::AddRule(unsigned type, double value)
+{
+	switch (type)
+	{
+	case 1:
+	{
+		if (/*Picked only 2 Points*/)
+		{
+			Point2Point rule(/*point's parameters*/p1x, p1y, p2x, p2y, _storage_of_coordinates.add(value));
+			_storage_of_constraints.add(rule);
+		}
+		return;
+	}
+	case 2:
+	{
+		if (/*Picked only Point and Segment*/)
+		{
+			DistanceFromPointToSection rule(/*point's and segment's parameters*/px, py, p1x, p1y, p2x, p2y, _storage_of_coordinates.add(value));
+			_storage_of_constraints.add(rule);
+		}
+		return;
+	}
+	case 3:
+	{
+		if (/*Picked only 3 Points or Point and Segment*/)
+		{
+			DistanceToTheLine rule(/*point's and segment's parameters*/px, py, p1x, p1y, p2x, p2y, _storage_of_coordinates.add(value));
+			_storage_of_constraints.add(rule);
+		}
+		return;
+	}
+	default:
+		return;
+	}
+}
+
 void CORE::ChangeStatus(double x, double y, unsigned char status_key)
 {
 	/*Try to search points in small radius*/
 	unsigned size = _storage_of_points.size();
-	double min = 5;
+	double min = 2;
 	int min_i = -1;
-	/*for (unsigned i = 0; i < size; i++)
+	for (unsigned i = 0; i < size; i++)
 	{
-		if (length(_storage_of_points[i].x, _storage_of_points[i].y, x, y) < min)
+		if (length(*_storage_of_points[i]._x, *_storage_of_points[i]._y, x, y) < min)
 		{
-			min = length(_storage_of_points[i].x, _storage_of_points[i].y, x, y);
+			min = length(*_storage_of_points[i]._x, *_storage_of_points[i]._y, x, y);
 			min_i = i;
 		}
-	}*/
+	}
 	size = _storage_of_segments.size();
 	int min_j = -1;
-	/*for (unsigned i = 0; i < size; i++)
+	for (unsigned i = 0; i < size; i++)
 	{
-		if (length(_storage_of_segments[i].o.x, _storage_of_segments[i].o.y, x, y) < min)
+		if (length(*_storage_of_segments[i]._p1->_x, *_storage_of_segments[i]._p1->_y, x, y) < min)
 		{
-			min = length(_storage_of_segments[i].o.x, _storage_of_segments[i].o.y, x, y);
+			min = length(*_storage_of_segments[i]._p1->_x, *_storage_of_segments[i]._p1->_y, x, y);
 			min_j = i;
 		}
-	}*/
+	}
+	size = _storage_of_circles.size();
+	int min_k = -1;
+	for (unsigned i = 0; i < size; i++)
+	{
+		if (length(*_storage_of_circles[i]._o->_x, *_storage_of_circles[i]._o->_y, x, y) < min)
+		{
+			min = length(*_storage_of_circles[i]._o->_x, *_storage_of_circles[i]._o->_y, x, y);
+			min_k = i;
+		}
+	}
 	/*size = _storage_of_arcs.size();
 	int min_k = -1;
 	for (unsigned i = 0; i < size; i++)
@@ -70,26 +176,92 @@ void CORE::ChangeStatus(double x, double y, unsigned char status_key)
 			min = length(_storage_of_arcs[i].o.x, _storage_of_arcs[i].o.y, x, y);
 			min_k = i;
 		}
-	}
+	}*/
 	if (min_k >= 0)
 	{
-		//change picking
+		_storage_of_circles[min_k].changeVisible();
 	}
 	else 
-	{*/
+	{
 		if (min_j >= 0)
 		{
-			//change picking
+			_storage_of_segments[min_j].changeVisible();
 		}
 		else
 		{
 			if (min_i >= 0)
 			{
-				//change picking
+				_storage_of_points[min_i].changeVisible();
 			}
 			else return;
 		}
-	//}
+	}
+	return;
+}
+
+void CORE::Select(double x, double y)
+{
+	/*Try to search points in small radius*/
+	unsigned size = _storage_of_points.size();
+	double min = 2;
+	int min_i = -1;
+	for (unsigned i = 0; i < size; i++)
+	{
+		if (length(*_storage_of_points[i]._x, *_storage_of_points[i]._y, x, y) < min)
+		{
+			min = length(*_storage_of_points[i]._x, *_storage_of_points[i]._y, x, y);
+			min_i = i;
+		}
+	}
+	size = _storage_of_segments.size();
+	int min_j = -1;
+	for (unsigned i = 0; i < size; i++)
+	{
+		if (length(*_storage_of_segments[i]._p1->_x, *_storage_of_segments[i]._p1->_y, x, y) < min)
+		{
+			min = length(*_storage_of_segments[i]._p1->_x, *_storage_of_segments[i]._p1->_y, x, y);
+			min_j = i;
+		}
+	}
+	size = _storage_of_circles.size();
+	int min_k = -1;
+	for (unsigned i = 0; i < size; i++)
+	{
+		if (length(*_storage_of_circles[i]._o->_x, *_storage_of_circles[i]._o->_y, x, y) < min)
+		{
+			min = length(*_storage_of_circles[i]._o->_x, *_storage_of_circles[i]._o->_y, x, y);
+			min_k = i;
+		}
+	}
+	/*size = _storage_of_arcs.size();
+	int min_k = -1;
+	for (unsigned i = 0; i < size; i++)
+	{
+	if (length(_storage_of_arcs[i].o.x, _storage_of_arcs[i].o.y, x, y) < min)
+	{
+	min = length(_storage_of_arcs[i].o.x, _storage_of_arcs[i].o.y, x, y);
+	min_k = i;
+	}
+	}*/
+	if (min_k >= 0)
+	{
+		_storage_of_circles[min_k].changeSelect();
+	}
+	else
+	{
+		if (min_j >= 0)
+		{
+			_storage_of_segments[min_j].changeSelect();
+		}
+		else
+		{
+			if (min_i >= 0)
+			{
+				_storage_of_points[min_i].changeSelect();
+			}
+			else return;
+		}
+	}
 	return;
 }
 
@@ -137,4 +309,48 @@ void CORE::ChangeStatus(double x1, double y1, double x2, double y2, unsigned cha
 		}
 	}
 	return;
+}
+
+void CORE::Select(double x1, double y1, double x2, double y2)
+{
+	// select points
+	unsigned size = _storage_of_points.size();
+	for (unsigned i = 0; i < size; i++)
+	{
+		if (isInArea(*_storage_of_points[i]._x, *_storage_of_points[i]._y, x1, y1, x2, y2))
+		{
+			_storage_of_points[i].changeSelect();
+			_selected_objects.add(_storage_of_points[i]);
+		}
+	}
+
+	// select segments
+	size = _storage_of_segments.size();
+	for (unsigned i = 0; i < size; i++)
+	{
+		if (isInArea(*_storage_of_segments[i]._p1->_x, *_storage_of_segments[i]._p1->_y, x1, y1, x2, y2) &&
+			isInArea(*_storage_of_segments[i]._p2->_x, *_storage_of_segments[i]._p2->_y, x1, y1, x2, y2))
+		{
+			_storage_of_segments[i].changeSelect();
+			_selected_objects.add(_storage_of_segments[i]);
+		}
+	}
+	size = _storage_of_circles.size();
+	for (unsigned i = 0; i < size; i++)
+	{
+		if (isInArea(*_storage_of_circles[i]._o->_x, *_storage_of_circles[i]._o->_y, x1, y1, x2, y2))
+		{
+			_storage_of_circles[i].changeSelect();
+			_selected_objects.add(_storage_of_circles[i]);
+		}
+	}
+	return;
+}
+
+void CORE::ClearSelection()
+{
+	for (ListViewer<ObjectSkin> i(_selected_objects); i.canMoveNext(); i.moveNext())
+	{
+		i.getValue().changeSelect(false);
+	}
 }
--- lib/core.h
+++ lib/core.h
@@ -3,6 +3,7 @@
 #include "structures.h"
 #include "storage_array.h"
 #include "storage_list.h"
+#include "global.h"
 
 class CORE
 {
@@ -11,19 +12,35 @@ private:
 	Storage_Array<Point> _storage_of_points;
 	Storage_Array<Segment> _storage_of_segments;
 	Storage_Array<Circle> _storage_of_circles;
+	Storage_List<IConstraint> _storage_of_constraints;
+	Storage_List<ObjectSkin> _selected_objects;
+	GUI* mygui;
+
+	void Redraw();
+	void BuildFigure(IConstraint*, Storage_Array<double*>*);
+	void MainCalculate();
 public:
 	CORE();
+	CORE(GUI* gui)
+	{
+		mygui = gui;
+	}
 	~CORE();
+	void ConnectGUI(GUI* gui)
+	{
+		mygui = gui;
+	}
+
 	void AddObject(double point_x, double point_y); // add point
 	void AddObject(double point_x1, double point_y1, double point_x2, double point_y2); // add segment, automatical adds 2 points
-	void AddObject(double point_x, double point_y, double radius); //add circle, automatical add center of circle
+	void AddObject(double point_x, double point_y, double radius); // add circle, automatical add center of circle
 	void AddObject(double point_x, double point_y, double vector_x, double vector_y, double angle);
-	/*void AddRule(Point &point_1, Point &point_2, unsigned rule, ...);
-	void AddRule(Point &point, Segment &segment, unsigned rule, ...);
-	void AddRule(Segment &segment_1, Segment &segment_2, unsigned rule, ...);
-	void AddRule(Segment &segment, Arc &arc, unsigned rule, ...);*/
-	void ChangeStatus(double x, double y, unsigned char status_key);
-	void ChangeStatus(double x1, double y1, double x2, double y2, unsigned char status_key);
+	void AddRule(unsigned type, double value);
+	void ChangeStatus(double x, double y, unsigned char status_key); // need refine
+	void ChangeStatus(double x1, double y1, double x2, double y2, unsigned char status_key); // need refine
+	void Select(double x, double y);
+	void Select(double x1, double y1, double x2, double y2);
+	void ClearSelection();
 	void ChangeProperty(unsigned property_key, ...);
 };
 
