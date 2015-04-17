aa0c8030970dc3fc4b2ee227b5f39d3253548e4a
77maxikov <77maxikov@mail.ru>
2015-04-17 18:19:45 +0300

    Constraint types as enums

--- include/constraints/global.h
+++ include/constraints/global.h
@@ -5,6 +5,14 @@ double length(double x1, double y1, double x2, double y2){
 	return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
 }
 
+enum CONSTR_TYPE{
+	CONSTR_P2PDIST,
+	CONSTR_P2SECTDIST,
+	CONSTR_P2LINEDIST,
+	CONSTR_3PONLINE,
+	CONSTR_L2LANGLE
+}; 
+
 class IConstraint
 {
 public:
