9863dff3afc5cc2be2d8c067799e71231fbecbe2
aserg24 <vudee@ya.ru>
2015-04-19 19:57:59 +0400

    add AspectRatio

--- constraint.h
+++ constraint.h
@@ -150,4 +150,50 @@ public:
 	}
 };
 
+class AspectRatio :public iConstraint
+{
+	double *_p1x, *_p1y, *_p2x, *_p2y, *_p3x, *_p3y, *_k;
+public:
+	AspectRatio(double *p1x, double *p1y, double *p2x, double *p2y, double *p3x, double *p3y, double *k)
+	{
+		_p1x = p1x;
+		_p1y = p1y;
+		_p2x = p2x;
+		_p2y = p2y;
+		_p3x = p3x;
+		_p3y = p3y;
+		_k = k;
+	}
+	double error()
+	{
+		return pow(*_k - length(*_p1x, *_p1y, *_p2x, *_p2y)/length(*_p2x, *_p2y, *_p3x, *_p3y),2);
+	}
+	double diff(double *par)
+	{
+		if (par == _p1x)
+			return (-2) * (*_k - length(*_p1x, *_p1y, *_p2x, *_p2y) / length(*_p2x, *_p2y, *_p3x, *_p3y)) *
+			(*_p1x - *_p2x) / (length(*_p2x, *_p2y, *_p3x, *_p3y)*length(*_p1x, *_p1y, *_p2x, *_p2y));
+		if (par == _p1y)
+			return (-2) * (*_k - length(*_p1x, *_p1y, *_p2x, *_p2y) / length(*_p2x, *_p2y, *_p3x, *_p3y)) *
+			(*_p1y - *_p2y) / (length(*_p2x, *_p2y, *_p3x, *_p3y)*length(*_p1x, *_p1y, *_p2x, *_p2y));
+		if (par == _p2x)
+			return (-2) * (*_k - length(*_p1x, *_p1y, *_p2x, *_p2y) / length(*_p2x, *_p2y, *_p3x, *_p3y)) *
+			length(*_p2x, *_p2y, *_p3x, *_p3y)*((*_p2x - *_p1x)*pow(length(*_p2x, *_p2y, *_p3x, *_p3y), 2) -
+			(*_p2x - *_p3x)*pow(length(*_p1x, *_p1y, *_p2x, *_p2y), 2)) / (length(*_p1x, *_p1y, *_p2x, *_p2y)*
+			pow(length(*_p2x, *_p2y, *_p3x, *_p3y), 4));
+		if (par == _p2y)
+			return (-2) * (*_k - length(*_p1x, *_p1y, *_p2x, *_p2y) / length(*_p2x, *_p2y, *_p3x, *_p3y)) *
+			length(*_p2x, *_p2y, *_p3x, *_p3y)*((*_p2y - *_p1y)*pow(length(*_p2x, *_p2y, *_p3x, *_p3y), 2) -
+			(*_p2y - *_p3y)*pow(length(*_p1x, *_p1y, *_p2x, *_p2y), 2)) / (length(*_p1x, *_p1y, *_p2x, *_p2y)*
+			pow(length(*_p2x, *_p2y, *_p3x, *_p3y), 4));
+		if (par == _p3x)
+			return 2 * length(*_p1x, *_p1y, *_p2x, *_p2y)*(*_p3x - *_p2x) / pow(length(*_p2x, *_p2y, *_p3x, *_p3y), 3);
+		if (par == _p3y)
+			return 2 * length(*_p1x, *_p1y, *_p2x, *_p2y)*(*_p3y - *_p2y) / pow(length(*_p2x, *_p2y, *_p3x, *_p3y), 3);
+		if (par == _k)
+			return 2 * (*_k - length(*_p1x, *_p1y, *_p2x, *_p2y) / length(*_p2x, *_p2y, *_p3x, *_p3y));
+		return 0;
+	}
+};
+
 #endif // CONSTRAINT_H
