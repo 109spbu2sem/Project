555c27c0a8d648755ede85f41258893f419d81b7
kik2306 <kik999@mail.ru>
2015-04-24 18:59:02 +0300

    Изменяется цвет кнопки

--- pr/pr/pr/Исходный код.cpp
+++ pr/pr/pr/Исходный код.cpp
@@ -2,6 +2,7 @@
 #include <FL\Fl_Window.H>
 #include <FL\Fl_Button.H>
 #include <FL\Fl_Input.H>
+#include <FL/Fl_Output.H>
 #include <iostream>
 
 
@@ -9,16 +10,27 @@
 class MyMainWindow:public Fl_Window{
 	Fl_Button *_btn1;
 	Fl_Input *_input1;
-
+	Fl_Output *_output1;
+	
+		
 		static void MyCallback(Fl_Widget* w,void*para){
 		std::cout<<"Hello "<<std::endl;
 		std::cout<<"para=  "<<( MyMainWindow*)para<<std::endl;
 		std::cout<<"string input=  "<<(( MyMainWindow*)para)->_input1->value()<<std::endl;
+		if (w->color() == 0xFFA81200) {
+			w->color(0xFF751800); //toggle
+		}
+			else {
+		w->color(0xFFA81200);//toggle
+		}
 }
 		 MyMainWindow(int w,int h,const char* caption):Fl_Window(w,h,caption){
-		_btn1=new Fl_Button(10,10,100,25,"Button1");
-		_input1=new Fl_Input(10,40,100,25);
+		_btn1=new Fl_Button(10,10,100,25,"Click me!");
+		_btn1->color(0xFFA81200);
+		_input1=new Fl_Input(90, 10, 180, 20);
+		_output1=new Fl_Output(90, 40, 180, 20, "Output : ");
 		_btn1->callback(MyCallback,this);
+
 	show();
 	}
 	static MyMainWindow *_singleton;
@@ -26,9 +38,11 @@ public:
 	
 	static MyMainWindow *getSingleton(){
 		if(_singleton==0)
-			_singleton=new MyMainWindow(100,100,"Main window");
-		return _singleton;
+			_singleton=new MyMainWindow(400,400,"Main window");
+			return _singleton;
 	}
+	
+	
 	void Draw(double point_x,double point_y,unsigned color=0);
 	void Draw(double point1_x,double point1_y,double point2_x,double point2_y);
 	void Draw(double point_x,double point_y,double radius);
@@ -37,8 +51,10 @@ public:
 MyMainWindow* MyMainWindow::_singleton=0;
 
 
+
 int main(int argc,char *argv[]){
 	 MyMainWindow *window1= MyMainWindow::getSingleton();
+	window1->color(FL_WHITE);
 	window1->show();
 	/*MyWindow  *button=new MyWindow (10,10,100,25,"Press");
 	
