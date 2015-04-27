9bbd6c1e8984dbd0beb166c724333879782f16c2
kik2306 <kik999@mail.ru>
2015-04-27 23:08:17 +0300

    text box

--- pr/pr/pr/Исходный код.cpp
+++ pr/pr/pr/Исходный код.cpp
@@ -2,7 +2,7 @@
 #include <FL\Fl_Window.H>
 #include <FL\Fl_Button.H>
 #include <FL\Fl_Input.H>
-#include <FL/Fl_Output.H>
+#include <FL\Fl_Output.H>
 #include <iostream>
 
 
@@ -14,22 +14,30 @@ class MyMainWindow:public Fl_Window{
 	
 		
 		static void MyCallback(Fl_Widget* w,void*para){
-		std::cout<<"Hello "<<std::endl;
-		std::cout<<"para=  "<<( MyMainWindow*)para<<std::endl;
-		std::cout<<"string input=  "<<(( MyMainWindow*)para)->_input1->value()<<std::endl;
-		if (w->color() == 0xFFA81200) {
-			w->color(0xFF751800); //toggle
+		
+		//std::cout<<"Hello "<<std::endl;
+		//std::cout<<"para=  "<<( MyMainWindow*)para<<std::endl;
+		//std::cout<<"string input=  "<<(( MyMainWindow*)para)->_input1->value()<<std::endl;
+			((MyMainWindow*)para)->_input1->label;
+			((MyMainWindow*)para)->_output1->value->_input1.value();
+			((MyMainWindow*)para)->_input1->value(""); //clear again
+
+		if (w->color() == 0xFFFFFF00) {
+			w->color(0xFF000000); //toggle
 		}
 			else {
-		w->color(0xFFA81200);//toggle
+		w->color(0xFFFFFF00);//toggle
 		}
 }
 		 MyMainWindow(int w,int h,const char* caption):Fl_Window(w,h,caption){
-		_btn1=new Fl_Button(10,10,100,25,"Click me!");
+		_btn1=new Fl_Button(130,200,100,25,"Click me!");
 		_btn1->color(0xFFA81200);
-		_input1=new Fl_Input(90, 10, 180, 20);
+		_input1=new Fl_Input(90, 10, 180, 20,"Output : ");
 		_output1=new Fl_Output(90, 40, 180, 20, "Output : ");
+		_input1->callback((Fl_Callback*)MyCallback, (void *)"Enter next:");
+		_input1->when(FL_WHEN_RELEASE |FL_WHEN_ENTER_KEY);
 		_btn1->callback(MyCallback,this);
+
 
 	show();
 	}
