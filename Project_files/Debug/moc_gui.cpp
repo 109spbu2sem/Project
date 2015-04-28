79af7576e3d3fa9bc62d9f7596feabf8132844d9
Vetcher <vetcher@ngs.ru>
2015-04-28 17:32:15 +0300

    0.0.1.2 delete this

--- Project_files/Debug/moc_gui.cpp
+++ Project_files/Debug/moc_gui.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[12];
-    char stringdata[246];
+    QByteArrayData data[13];
+    char stringdata[269];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,8 +39,9 @@ QT_MOC_LITERAL(6, 99, 23), // "on_calculateBTN_clicked"
 QT_MOC_LITERAL(7, 123, 30), // "on_ruleBox_currentIndexChanged"
 QT_MOC_LITERAL(8, 154, 21), // "on_pushButton_clicked"
 QT_MOC_LITERAL(9, 176, 23), // "on_pushButton_2_clicked"
-QT_MOC_LITERAL(10, 200, 21), // "on_saveButton_clicked"
-QT_MOC_LITERAL(11, 222, 23) // "on_saveAsButton_clicked"
+QT_MOC_LITERAL(10, 200, 25), // "on_concatinateBTN_clicked"
+QT_MOC_LITERAL(11, 226, 21), // "on_selectTool_clicked"
+QT_MOC_LITERAL(12, 248, 20) // "on_pointTool_clicked"
 
     },
     "MainWindow\0on_openAddingBTN_clicked\0"
@@ -49,7 +50,8 @@ QT_MOC_LITERAL(11, 222, 23) // "on_saveAsButton_clicked"
     "index\0on_calculateBTN_clicked\0"
     "on_ruleBox_currentIndexChanged\0"
     "on_pushButton_clicked\0on_pushButton_2_clicked\0"
-    "on_saveButton_clicked\0on_saveAsButton_clicked"
+    "on_concatinateBTN_clicked\0"
+    "on_selectTool_clicked\0on_pointTool_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +61,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       9,   14, // methods
+      10,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,15 +69,16 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   59,    2, 0x08 /* Private */,
-       3,    0,   60,    2, 0x08 /* Private */,
-       4,    1,   61,    2, 0x08 /* Private */,
-       6,    0,   64,    2, 0x08 /* Private */,
-       7,    1,   65,    2, 0x08 /* Private */,
-       8,    0,   68,    2, 0x08 /* Private */,
-       9,    0,   69,    2, 0x08 /* Private */,
-      10,    0,   70,    2, 0x08 /* Private */,
-      11,    0,   71,    2, 0x08 /* Private */,
+       1,    0,   64,    2, 0x08 /* Private */,
+       3,    0,   65,    2, 0x08 /* Private */,
+       4,    1,   66,    2, 0x08 /* Private */,
+       6,    0,   69,    2, 0x08 /* Private */,
+       7,    1,   70,    2, 0x08 /* Private */,
+       8,    0,   73,    2, 0x08 /* Private */,
+       9,    0,   74,    2, 0x08 /* Private */,
+      10,    0,   75,    2, 0x08 /* Private */,
+      11,    0,   76,    2, 0x08 /* Private */,
+      12,    0,   77,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -83,6 +86,7 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::Int,    5,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -103,8 +107,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 4: _t->on_ruleBox_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 5: _t->on_pushButton_clicked(); break;
         case 6: _t->on_pushButton_2_clicked(); break;
-        case 7: _t->on_saveButton_clicked(); break;
-        case 8: _t->on_saveAsButton_clicked(); break;
+        case 7: _t->on_concatinateBTN_clicked(); break;
+        case 8: _t->on_selectTool_clicked(); break;
+        case 9: _t->on_pointTool_clicked(); break;
         default: ;
         }
     }
@@ -126,6 +131,8 @@ void *MainWindow::qt_metacast(const char *_clname)
     if (!_clname) return Q_NULLPTR;
     if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
         return static_cast<void*>(const_cast< MainWindow*>(this));
+    if (!strcmp(_clname, "Interface"))
+        return static_cast< Interface*>(const_cast< MainWindow*>(this));
     return QMainWindow::qt_metacast(_clname);
 }
 
@@ -135,13 +142,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 9)
+        if (_id < 10)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 9;
+        _id -= 10;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 9)
+        if (_id < 10)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 9;
+        _id -= 10;
     }
     return _id;
 }
