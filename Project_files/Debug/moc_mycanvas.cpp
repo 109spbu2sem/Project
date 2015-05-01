59be7fc7f53e53a3e91d7b6fdfe72a32e730a204
Vetcher <vetcher@ngs.ru>
2015-05-02 01:10:07 +0300

    0.0.1.9 Interface changes

--- Project_files/Debug/moc_mycanvas.cpp
+++ Project_files/Debug/moc_mycanvas.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MyCanvas_t {
-    QByteArrayData data[5];
-    char stringdata[45];
+    QByteArrayData data[6];
+    char stringdata[67];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,11 +33,12 @@ QT_MOC_LITERAL(0, 0, 8), // "MyCanvas"
 QT_MOC_LITERAL(1, 9, 15), // "mousePressEvent"
 QT_MOC_LITERAL(2, 25, 0), // ""
 QT_MOC_LITERAL(3, 26, 12), // "QMouseEvent*"
-QT_MOC_LITERAL(4, 39, 5) // "event"
+QT_MOC_LITERAL(4, 39, 5), // "event"
+QT_MOC_LITERAL(5, 45, 21) // "mouseDoubleClickEvent"
 
     },
     "MyCanvas\0mousePressEvent\0\0QMouseEvent*\0"
-    "event"
+    "event\0mouseDoubleClickEvent"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +48,7 @@ static const uint qt_meta_data_MyCanvas[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,9 +56,11 @@ static const uint qt_meta_data_MyCanvas[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   19,    2, 0x0a /* Public */,
+       1,    1,   24,    2, 0x0a /* Public */,
+       5,    1,   27,    2, 0x0a /* Public */,
 
  // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
     QMetaType::Void, 0x80000000 | 3,    4,
 
        0        // eod
@@ -69,6 +72,7 @@ void MyCanvas::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         MyCanvas *_t = static_cast<MyCanvas *>(_o);
         switch (_id) {
         case 0: _t->mousePressEvent((*reinterpret_cast< QMouseEvent*(*)>(_a[1]))); break;
+        case 1: _t->mouseDoubleClickEvent((*reinterpret_cast< QMouseEvent*(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -99,13 +103,13 @@ int MyCanvas::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 2)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 2;
     }
     return _id;
 }
